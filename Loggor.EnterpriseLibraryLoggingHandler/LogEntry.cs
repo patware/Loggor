115743c3b92398aa9d3af72adf432dfb76c30110
Patrice Calvé <patware@hotmail.com>
2014-12-16 07:28:15 -0500

    Adding EnterpriseLibraryLoggingHandler

---
+++ Loggor.EnterpriseLibraryLoggingHandler/LogEntry.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loggor.EnterpriseLibraryLoggingHandler
+{
+    class LogEntry : Loggor.Lib.LogEntryBase, Loggor.Lib.ILogEntry
+    {
+
+        public Microsoft.Practices.EnterpriseLibrary.Logging.LogEntry Entry { get; private set; }
+
+        public LogEntry()
+        {
+            this.Entry = new Microsoft.Practices.EnterpriseLibrary.Logging.LogEntry();
+        }
+        
+        #region ILogEntry Implementation
+        Guid Lib.ILogEntry.ActivityId
+        {
+            get
+            {
+                return this.Entry.ActivityId;
+            }
+            set
+            {
+                this.Entry.ActivityId = value;
+            }
+        }
+
+        string Lib.ILogEntry.ActivityIdString
+        {
+            get { return this.Entry.ActivityIdString; }
+        }
+
+        void Lib.ILogEntry.AddErrorMessage(string message)
+        {
+            this.Entry.AddErrorMessage(message);
+        }
+
+        string Lib.ILogEntry.AppDomainName
+        {
+            get
+            {
+                return this.Entry.AppDomainName;
+            }
+            set
+            {
+                this.Entry.AppDomainName = value;
+            }
+        }
+
+        ICollection<string> Lib.ILogEntry.Categories
+        {
+            get
+            {
+                return this.Entry.Categories;
+            }
+            set
+            {
+                this.Entry.Categories = value;
+            }
+        }
+
+        string[] Lib.ILogEntry.CategoriesStrings
+        {
+            get { return this.Entry.CategoriesStrings; }
+        }
+
+        string Lib.ILogEntry.ErrorMessages
+        {
+            get { return this.Entry.ErrorMessages; }
+        }
+
+        int Lib.ILogEntry.EventId
+        {
+            get
+            {
+                return this.Entry.EventId;
+            }
+            set
+            {
+                this.Entry.EventId = value;
+            }
+        }
+
+        IDictionary<string, object> Lib.ILogEntry.ExtendedProperties
+        {
+            get
+            {
+                return this.Entry.ExtendedProperties;
+            }
+            set
+            {
+                this.Entry.ExtendedProperties = value;
+            }
+        }
+
+        string Lib.ILogEntry.LoggedSeverity
+        {
+            get { return this.Entry.LoggedSeverity; }
+        }
+
+        string Lib.ILogEntry.MachineName
+        {
+            get
+            {
+                return this.Entry.MachineName;
+            }
+            set
+            {
+                this.Entry.MachineName = value;
+            }
+        }
+
+        string Lib.ILogEntry.ManagedThreadName
+        {
+            get
+            {
+                return this.Entry.ManagedThreadName;
+            }
+            set
+            {
+                this.Entry.ManagedThreadName = value;
+            }
+        }
+
+        string Lib.ILogEntry.Message
+        {
+            get
+            {
+                return this.Entry.Message;
+            }
+            set
+            {
+                this.Entry.Message = value;
+            }
+        }
+
+        int Lib.ILogEntry.Priority
+        {
+            get
+            {
+                return this.Entry.Priority;
+            }
+            set
+            {
+                this.Entry.Priority = value;
+            }
+        }
+
+        string Lib.ILogEntry.ProcessId
+        {
+            get
+            {
+                return this.Entry.ProcessId;
+            }
+            set
+            {
+                this.Entry.ProcessId = value;
+            }
+        }
+
+        string Lib.ILogEntry.ProcessName
+        {
+            get
+            {
+                return this.Entry.ProcessName;
+            }
+            set
+            {
+                this.Entry.ProcessName = value;
+            }
+        }
+
+        Guid? Lib.ILogEntry.RelatedActivityId
+        {
+            get
+            {
+                return this.Entry.RelatedActivityId;
+            }
+            set
+            {
+                this.Entry.RelatedActivityId = value;
+            }
+        }
+
+        TraceEventType Lib.ILogEntry.Severity
+        {
+            get
+            {
+                return this.Entry.Severity;
+            }
+            set
+            {
+                this.Entry.Severity = value;
+            }
+        }
+
+        DateTime Lib.ILogEntry.TimeStamp
+        {
+            get
+            {
+                return this.Entry.TimeStamp;
+            }
+            set
+            {
+                this.Entry.TimeStamp = value;
+            }
+        }
+
+        string Lib.ILogEntry.TimeStampString
+        {
+            get { return this.Entry.TimeStampString; }
+        }
+
+        string Lib.ILogEntry.Title
+        {
+            get
+            {
+                return this.Entry.Title;
+            }
+            set
+            {
+                this.Entry.Title = value;
+            }
+        }
+
+        string Lib.ILogEntry.Win32ThreadId
+        {
+            get
+            {
+                return this.Entry.Win32ThreadId;
+            }
+            set
+            {
+                this.Entry.Win32ThreadId = value;
+            }
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Loggor.EnterpriseLibraryLoggingHandler/LogWriter.cs
@@ -0,0 +1,185 @@
+﻿using Loggor.Lib;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loggor.EnterpriseLibraryLoggingHandler
+{
+    public class LogWriter : Loggor.Lib.LogWriterBase, Loggor.Lib.ILogWriter
+    {
+
+        public Microsoft.Practices.EnterpriseLibrary.Logging.LogWriter Writer {get; private set;}
+        public LogWriter()
+        {
+            var config = new Microsoft.Practices.EnterpriseLibrary.Logging.LoggingConfiguration();
+            this.Writer = new Microsoft.Practices.EnterpriseLibrary.Logging.LogWriter(config);
+        }
+
+
+        #region LogWriterBase Implementation
+        public override ShortLive NewShortLiveLog(int startEventId, string startMessage, int stopEventId, string stopMessage, string title, params object[] args)
+        {
+            var sl = new Loggor.Lib.ShortLive(this, startEventId, startMessage, stopEventId, stopMessage, title, args);
+            return sl;
+
+        }
+        protected override ILogEntry GetNewLog()
+        {
+            var nl = new LogEntry();
+
+            return nl;
+
+        }
+        protected override bool IsLoggingEnabled()
+        {
+            return this.Writer.IsLoggingEnabled();
+        }
+
+        protected override bool ShouldLog(ILogEntry logEntry)
+        {
+            var le = logEntry as LogEntry;
+            if (le != null)
+                return this.Writer.ShouldLog(le.Entry);
+            else
+                return false;
+        }
+
+        protected override void Write(ILogEntry logEntry)
+        {
+            var le = logEntry as LogEntry;
+            if (le != null)
+                this.Writer.Write(le.Entry);
+        }
+        #endregion
+        
+
+        #region ILogWriter Implementation
+        bool Lib.ILogWriter.IsLoggingEnabled()
+        {
+            return this.Writer.IsLoggingEnabled();
+        }
+
+        bool Lib.ILogWriter.IsTracingEnabled()
+        {
+            return this.Writer.IsTracingEnabled();
+        }
+
+        bool Lib.ILogWriter.ShouldLog(Lib.ILogEntry log)
+        {
+            var entLibEntry = log as Loggor.EnterpriseLibraryLoggingHandler.LogEntry;
+
+            return this.Writer.ShouldLog(entLibEntry.Entry);
+        }
+        Lib.ILogEntry Lib.ILogWriter.NewLog()
+        {
+            var le = new LogEntry();
+
+            return le;
+        }
+
+        void Lib.ILogWriter.Write(Lib.ILogEntry log)
+        {
+            var entLibEntry = log as Loggor.EnterpriseLibraryLoggingHandler.LogEntry;
+
+            this.Writer.Write(entLibEntry.Entry);
+        }
+
+        void Lib.ILogWriter.Write(object message)
+        {
+            this.Writer.Write(message);
+        }
+
+        void Lib.ILogWriter.Write(object message, IDictionary<string, object> properties)
+        {
+            this.Writer.Write(message, properties);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories)
+        {
+            this.Writer.Write(message, categories);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories, IDictionary<string, object> properties)
+        {
+            this.Writer.Write(message, categories, properties);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories, int priority)
+        {
+            this.Writer.Write(message, categories, priority);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories, int priority, IDictionary<string, object> properties)
+        {
+            this.Writer.Write(message, categories, priority, properties);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories, int priority, int eventId)
+        {
+            this.Writer.Write(message, categories, priority, eventId);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories, int priority, int eventId, TraceEventType severity)
+        {
+            this.Writer.Write(message, categories, priority, eventId, severity);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories, int priority, int eventId, TraceEventType severity, string title)
+        {
+            this.Writer.Write(message, categories, priority, eventId, severity, title);
+        }
+
+        void Lib.ILogWriter.Write(object message, IEnumerable<string> categories, int priority, int eventId, TraceEventType severity, string title, IDictionary<string, object> properties)
+        {
+            this.Writer.Write(message,categories, priority, eventId, severity, title, properties);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category)
+        {
+            this.Writer.Write(message, category);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category, IDictionary<string, object> properties)
+        {
+            this.Writer.Write(message, category, properties);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category, int priority)
+        {
+            this.Writer.Write(message, category, priority);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category, int priority, IDictionary<string, object> properties)
+        {
+            this.Writer.Write(message, category, priority, properties);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category, int priority, int eventId)
+        {
+            this.Writer.Write(message, category, priority, eventId);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category, int priority, int eventId, TraceEventType severity)
+        {
+            this.Writer.Write(message, category, priority, eventId, severity);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category, int priority, int eventId, TraceEventType severity, string title)
+        {
+            this.Writer.Write(message, category, priority, eventId, severity, title);
+        }
+
+        void Lib.ILogWriter.Write(object message, string category, int priority, int eventId, TraceEventType severity, string title, IDictionary<string, object> properties)
+        {
+            this.Writer.Write(message, category, priority, eventId, severity, title, properties);
+        }
+        #endregion
+
+
+
+    }
+}
